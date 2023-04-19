b3e5f8e181b6cfe7e93aa519b8c6959deeecb336
Yuri Arbeletche <ysba000@gmail.com>
2023-04-19 12:01:06 -0300

    adicionado main.h

---
+++ User/main.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "NuMicro.h"
+
+#define BOOTLOADER_VERSION  		0x0200
+
+#define BOOT_TRANSFER_BASE	0x20000
+#define BOOT_FIRMWARE_LEN 	0x20000
+
+#define FLASH_FIRMWARE_LEN  0x214
+
+
+#define FLASH_BOOTLOADER_VERSION    0x100feC
